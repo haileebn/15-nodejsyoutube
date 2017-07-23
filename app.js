096499ed8117eb8c35baa7bebc6df4d37a64fc2b
haileebn <hainvyp@gmail.com>
2017-07-23 10:32:56 +0700

    #8

---
+++ app.js
@@ -0,0 +1,5 @@
+const stuff = require('./stuff');
+
+console.log(stuff.counter(['hai', 'nguyen', 'van']));
+console.log(stuff.pi);
+console.log(stuff.adder(2,3));
---
+++ stuff.js
@@ -0,0 +1,15 @@
+let counter = function(arr) {
+  return arr.length;
+};
+
+let adder = function(a, b) {
+  return `Sum = ${a+b}`;
+};
+
+let pi = 3.14;
+
+module.exports = {
+  counter: counter,
+  adder: adder,
+  pi: pi
+};
