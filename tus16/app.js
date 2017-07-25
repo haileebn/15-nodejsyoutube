86f070fa73bba0a1ac268341d4e99c5d08e8f7b5
haileebn <hainvyp@gmail.com>
2017-07-25 21:26:19 +0700

    #19

--- tus16/app.js
+++ tus16/app.js
@@ -2,11 +2,21 @@ let fs = require('fs');
 let http = require('http');
 let server = http.createServer(function(req, res) {
 	// body...
-	let myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
-	let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')	
+	console.log('request was made: ' + req.url);
+	let myReadStream = fs.createReadStream(__dirname + '/index.html');
+	// let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')	
 	
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	myReadStream.pipe(res);
+	// res.writeHead(200, {'Content-Type': 'text/plain'});
+	// res.writeHead(200, {'Content-Type': 'text/html'});
+	res.writeHead(200, {'Content-Type': 'application/JSON'});
+
+	var myobj = {
+		name: 'hai',
+		age: 21
+	}
+	// console.log(myobj);
+	// myReadStream.pipe(res);
+	res.end(JSON.stringify(myobj));
 });
 
 
@@ -17,6 +27,9 @@ let server = http.createServer(function(req, res) {
 // });
 // myReadStream.pipe(myWriteStream);
 
-server.listen(3000);
+server.listen(3000, function() {
+	// body...
+	console.log('Start server: 3000.');
+});
 
 
