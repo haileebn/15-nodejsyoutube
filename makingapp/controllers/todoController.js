4324347a2dcc0c662417233ffca3c2de4ea50da7
haileebn <hainvyp@gmail.com>
2017-07-27 16:56:16 +0700

    part3 4 #tus33,34

--- makingapp/controllers/todoController.js
+++ makingapp/controllers/todoController.js
@@ -1,15 +1,25 @@
+let bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+let data = [{item: 'get milk'},{item: 'walk dog'}, {item: 'kick some coding ass'}]
+
 module.exports = function(app) {
 	// body...
 	app.get('/todo', function(req, res) {
 		// body...
-		res.render('todo');
+		res.render('todo', {todos: data});
 	});
-	app.post('/todo', function(req, res) {
+	app.post('/todo', urlencodedParser, function(req, res) {
 		// body...
-		
+		data.push(req.body)
+		res.json(data);
 	});
-	app.delete('/todo', function(req, res) {
+	app.delete('/todo/:item', function(req, res) {
 		// body...
-		
+		data = data.filter(function(todo) {
+			return todo.item.replace(/ /g, '-') !== req.params.item;			
+		});
+	res.json(data);
 	});
 }
