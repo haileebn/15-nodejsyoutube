e9a26248468ed6a6819d4ac4f4d3de80248a4989
haileebn <hainvyp@gmail.com>
2017-07-27 09:52:35 +0700

    #27partial template

--- tus16/index.js
+++ tus16/index.js
@@ -5,16 +5,26 @@ let port = process.env.PORT || 3000;
 let app = express();
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+	/*optional stuff to do after success */
+	res.render('index');
+});
+
+app.get('/contact', function(req, res) {
+	/*optional stuff to do after success */
+	res.render('contact');
+});
+
 app.get('/profile/:name', function(req, res) {
 	// body...
 	let myobj = {age: 20, job: 'DEV', hobbies: ['eating', 'fighting', 'fishing']};
 	res.render('profile', {person: req.params.name, data: myobj});
 });
 
-app.get('/contact', function(req, res) {
-	/*optional stuff to do after success */
-	res.send('This is the contact page.');
-});
+// app.get('/contact', function(req, res) {
+// 	optional stuff to do after success 
+// 	res.send('This is the contact page.');
+// });
 
 app.listen(port,function() {
 	// body...
